06849e67bcadd4f47b6e3c15b5de34e9e724366c
Anldmrlpp <147911313+Anldmrlpp@users.noreply.github.com>
2024-08-01 00:46:41 +0300

    ContactServiceImpl eklendi

--- src/main/java/com/StajProject/Company/service/impl/ContactServiceImpl.java
+++ src/main/java/com/StajProject/Company/service/impl/ContactServiceImpl.java
@@ -4,11 +4,15 @@ import com.StajProject.Company.dto.ContactCreateDto;
 import com.StajProject.Company.dto.ContactDto;
 import com.StajProject.Company.dto.ContactUpdateDto;
 import com.StajProject.Company.service.ContactService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
+@Service
 public class ContactServiceImpl implements ContactService {
     @Override
     public UUID createContact(ContactCreateDto contactCreateDto) {
